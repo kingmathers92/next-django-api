e9b1745ef41a4179f5a926a61543c9f58bd46ab7
KHALED\E392 <khaledb.yahya@gmail.com>
2024-07-16 02:20:08 +0100

    auth with django ninja

---
+++ src/cfehome/api.py
@@ -0,0 +1,25 @@
+from ninja import NinjaAPI, Schema
+
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.controller import NinjaJWTDefaultController
+
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
+
+
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+    # is not request.user.is_authenticated
+    email: str = None
+
+@api.get("/hello")
+def hello(request):
+    print(request)
+    return "Hello World"
+
+@api.get("/me", response=UserSchema, auth=JWTAuth())
+def hello(request):
+    print(request)
+    return request.user
--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "ninja_extra",
+    "jwt",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NINJA_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
