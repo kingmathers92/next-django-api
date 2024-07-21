186312dcf477b2e06a20cfac210137bee62c4cd8
KHALED\E392 <khaledb.yahya@gmail.com>
2024-07-21 17:54:26 +0100

    user foreign keys

--- src/waitlist/models.py
+++ src/waitlist/models.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL # "auth.user"
 
 # Create your models here.
 class WaitlistEntry(models.Model):
     # user
+    user = models.ForeignKey(User, default=None, null=True, blank=True, on_delete=models.SET_NULL)
     email = models.EmailField()
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
