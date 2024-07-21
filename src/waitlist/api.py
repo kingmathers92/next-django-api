9707deabb33f95421509379f29165f6d9077cab3
KHALED\E392 <khaledb.yahya@gmail.com>
2024-07-21 17:27:00 +0100

    create obj with POST

--- src/waitlist/api.py
+++ src/waitlist/api.py
@@ -3,14 +3,24 @@ from django.shortcuts import get_object_or_404
 from ninja import Router
 from ninja_jwt.authentication import JWTAuth
 from .models import WaitlistEntry
-from .schemas import WaitlistEntryListSchema, WaitlistEntryDetailSchema
+from .schemas import WaitlistEntryCreateSchema, WaitlistEntryListSchema, WaitlistEntryDetailSchema
 
 router = Router()
 
+# /api/waitlist
 @router.get("", response=List[WaitlistEntryListSchema], auth=JWTAuth())
 def list_waitlist_entries(request):
     qs = WaitlistEntry.objects.all()
     return qs
+
+# /api/waitlist
+@router.post("", response=WaitlistEntryDetailSchema)
+def create_waitlist_entry(request, data:WaitlistEntryCreateSchema):
+    ##print(data)
+    obj =  WaitlistEntry.objects.create(**data.dict())
+    print(request.user)
+    return obj
+
 
 @router.get("{entry_id}", response=WaitlistEntryDetailSchema)
 def get_waitlist_entry(request, entry_id:int):
